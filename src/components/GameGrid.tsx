845566bbb5e47ade07b7c79b70ae94b6ce22fd66
kiwioyster <kiwioyster@gmail.com>
2020-11-02 17:52:16 +1300

    remove unused imports

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 import Tile from './Tile';
 import styled from 'styled-components';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { highlightTiles, removeTiles } from '../store/actionCreators';
 import tileMatch from '../utils/tileMatch';
 import Modal from './Modal';
